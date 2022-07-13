62d192ed2ea2ac7051f658bc33333c5042b5e12e
Miller <farwes1180@gmail.com>
2022-07-14 00:31:22 +0900

    Class

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,36 +1,60 @@
-// Functions (optional and default parameters)
+class Robot {
+  private _color: string;
 
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-type MyFunc = (a: number, b: number) => number;
-const sum2: MyFunc = (a, b) => a + b;
-
-sum(1, 1);
-
-type MyFunc2 = (a?: number, b?: number) => number;
-const sum3: MyFunc2 = (a = 0, b = 0) => a + b;
-
-sum3();
-
-// Unknown number of arguments
-
-function sumEveryThing(
-  arg1: string,
-  arg2: boolean,
-  ...numbers: number[]
-): number {
-  return numbers.reduce((result, sum) => result + sum, 0);
-}
-
-// Overloads
-function calcArea(width: number, height: number): number;
-function calcArea(length: number): number;
-function calcArea(...args: number[]): number {
-  if (args.length === 2) {
-    return args[0] * args[1];
+  static availableColors = ['green', 'yellow'];
+  static isColorAvailable(color: string) {
+    return Robot.availableColors.includes(color);
   }
 
-  return Math.pow(args[0], 2);
+  constructor(protected _name: string) {}
+
+  askName() {
+    console.log(`My name is ${this.name}`);
+  }
+
+  move(distance: number) {
+    console.log(`${this.name} moved ${distance} meters`);
+  }
+
+  set color(color: string) {
+    if (!Robot.isColorAvailable(color)) {
+      throw new Error(`Color ${color} is not available`);
+    }
+    this._color = color;
+  }
+
+  set name(value: string) {
+    this._name = 'PREFIX_' + value;
+  }
+
+  get name() {
+    return this._name + '_SUFFIX';
+  }
 }
+
+class FlyingRobot extends Robot {
+  jetpackSize: number;
+
+  constructor(name: string, jetpackSize: number) {
+    super(name);
+    this.jetpackSize = jetpackSize;
+  }
+
+  move(distance: number) {
+    console.log(`${this.name} is flying`);
+    super.move(distance);
+  }
+}
+
+const robot = new Robot('John');
+robot.askName();
+
+robot.move(30);
+
+const flyingRobot = new FlyingRobot('Jim', 2);
+flyingRobot.move(10);
+
+flyingRobot.name = 'Joe';
+console.log(`My name is ${flyingRobot.name}`);
+
+Robot.availableColors;
