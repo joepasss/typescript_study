251e2217d76b484f07e095455b8fddfabf5e8109
Miller <farwes1180@gmail.com>
2022-07-23 01:46:48 +0900

    ECMAScript Private Fields

---
+++ simple-typescript/src/ECMAScript-private-fields.ts
@@ -0,0 +1,28 @@
+export class Robot {
+  #name: string;
+
+  private somePrivateField = 'it is private Field!!!!';
+
+  constructor(name: string) {
+    this.#name = name;
+  }
+
+  getName() {
+    return this.#name;
+  }
+}
+
+export class AdvancedRobot extends Robot {
+  #name: string;
+
+  private somePrivateField = 'OVERRIDDEN PRIVATE FIELD';
+
+  constructor(name: string) {
+    super(name);
+    this.#name = `Advanced ${name}`;
+  }
+
+  getAdvancedRobotName() {
+    return this.#name;
+  }
+}
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import { AdvancedRobot } from './ECMAScript-private-fields';
+
+const robot = new AdvancedRobot('Jack');
+
+// it catches error but logged correctly
+console.log('Private : ', robot.somePrivateField);
+console.log('parent name : ', robot.getName());
+console.log('subclass name : ', robot.getAdvancedRobotName());
