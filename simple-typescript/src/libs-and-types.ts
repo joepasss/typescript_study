8eb7d0382ba0b1fc10e9e673e1c0a4a964a47165
Miller <farwes1180@gmail.com>
2022-07-24 04:44:17 +0900

    Type Alias & Using External Packages and Their Types

---
+++ simple-typescript/src/libs-and-types.ts
@@ -0,0 +1,9 @@
+// npm i -D @types/npm-pakage-name
+
+import ApolloClient from 'apollo-client';
+import * as React from 'react'; // no information of type
+// after npm i --save-dev @types/react errors disapper
+
+// or use declare module 'react'
+
+React.someFn();
